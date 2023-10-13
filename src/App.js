9805983587edf886c69d0fce3c1e0a562d77d3b1
hrishivuk <hrishivuk@gmail.com>
2023-10-14 00:28:26 +0530

    mnig

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          We start?
+          We start? mnig
         </p>
         <a
           className="App-link"
