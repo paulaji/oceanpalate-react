80836d4f97aa328818459f4c768c488d4d89743a
Saurav T Ajith <sauravtajith@gmail.com>
2023-10-14 00:40:46 +0530

    f

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         Welcome to Ocean Palate.
       </a>
       <p>We will start the app work now.</p>
-      <p>trueghost gay</p>
+      <p>trueghost is a very nice person</p>
       <p>This will become an old push by trueghost </p>
     </div>
   );
